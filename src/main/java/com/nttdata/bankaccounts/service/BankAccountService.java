dc2d9ef1a00e44dad76072a6ee0111a3b10ad977
Roram <68767119+Roram-io@users.noreply.github.com>
2022-01-10 22:50:02 -0500

    Created controller

---
+++ src/main/java/com/nttdata/bankaccounts/service/BankAccountService.java
@@ -0,0 +1,20 @@
+package com.nttdata.bankaccounts.service;
+
+import com.nttdata.bankaccounts.model.BankAccount;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface BankAccountService {
+
+    Flux<BankAccount> listBankAccounts();
+
+    Mono<BankAccount> listById(String id);
+
+    Mono<BankAccount> listByHolder(String id);
+
+    Mono<Void> removeBankAccount(String id);
+
+    Mono<BankAccount> saveBankAccount(BankAccount bankAccount);
+
+    Mono<BankAccount> updateBankAccount(BankAccount bankAccount);
+}
