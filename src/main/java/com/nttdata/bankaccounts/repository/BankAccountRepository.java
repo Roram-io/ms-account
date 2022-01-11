7d8c93c4597491da6b772428797cd24b1016d51d
Roram <68767119+Roram-io@users.noreply.github.com>
2022-01-11 01:00:10 -0500

    Revert "Feature/controller"

--- src/main/java/com/nttdata/bankaccounts/repository/BankAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nttdata.bankaccounts.repository;
-
-import com.nttdata.bankaccounts.model.BankAccount;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class BankAccountRepository extends ReactiveMongoRepository<BankAccount, String> {
-}
